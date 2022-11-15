499416d6c333cac02d876854df65699250830070
kschipulring <karl@3ringprototype.com>
2022-11-15 14:25:10 -0500

    IRD-1 -a more complete person.get(...) method definition

--- person.py
+++ person.py
@@ -1,11 +1,15 @@
+import db_core
 import boto3
+from boto3.dynamodb.conditions import Key, Attr
 
-def get(id):
-        
-    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+#just one person. Optional second parameter for when you only want certain attributes
+def get(id, attribs=[]):
+    table = db_core.getMainTable()
     
-    table = dynamodb.Table('incident_reports')
-    
-    r = table.get_item(Key={'person_id': id, 'record_type': 'person'}, AttributesToGet=['last_name'])
+    #if we just want certain attributes returned from DynamoDB
+    if len(attribs) > 0:
+        r = table.get_item(Key={'person_id': id, 'record_type': 'person'}, AttributesToGet=attribs)
+    else:
+        r = table.get_item(Key={'person_id': id, 'record_type': 'person'})
     
     return r
