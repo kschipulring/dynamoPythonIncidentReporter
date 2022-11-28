d4aa6a917740849c7ca2a29e1cb737cd3d4d5863
Karl Schipul <karl@3ringprototype.com>
2022-11-28 07:17:15 -0500

    IRD-1 - move database access files to subdirectory

---
+++ ddb/db_core.py
@@ -0,0 +1,17 @@
+import boto3
+import ddb.install_table as install_table
+
+#gets the core table for the app for use in calls by the other python fi
+def getMainTable():
+    
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+
+    table_name = 'incident_reports'
+
+    try:
+        table = dynamodb.Table(table_name)
+    except:
+        #install if not already existing
+        table = install_table.install(table_name)
+    
+    return table
---
+++ ddb/install_table.py
@@ -0,0 +1,59 @@
+import boto3
+from boto3.dynamodb.conditions import Key
+from boto3.dynamodb.types import STRING
+
+#can use a different table name if desired
+def install(table_name="incident_reports"):
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    
+    table = dynamodb.create_table(
+        TableName=table_name,
+        KeySchema=[
+            {'AttributeName': 'person_id', 'KeyType': 'HASH'},  # Partition key
+            {'AttributeName': 'record_type', 'KeyType': 'RANGE'}  # Sort key
+        ],
+        AttributeDefinitions=[
+            {'AttributeName': 'person_id', 'AttributeType': STRING},
+            {'AttributeName': 'record_type', 'AttributeType': STRING}
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 5,
+            'WriteCapacityUnits': 5,
+        }
+    )
+
+    #make sure it finishes being set up before returning. NOTE: should not be too long with this one.
+    table.wait_until_exists()
+
+
+    ## Now add a Global Secondary Index so that we can choose only people or only institutions, roles, etc. if we wanted to.
+
+    # Attrs for GSI
+    attrdef = [
+        {"AttributeName": "record_type", "AttributeType": STRING}
+    ]
+
+    # Asset ID index definition
+    recordTypeIdx = [
+        {
+            "Create": {
+                "IndexName": "record_type-index",
+                "KeySchema": [{
+                    "AttributeName": "record_type",
+                    "KeyType": "HASH"
+                }],
+                'Projection': {
+                    'ProjectionType': "ALL",
+                },
+                'ProvisionedThroughput': {
+                    'ReadCapacityUnits': 5,
+                    'WriteCapacityUnits': 5
+                }
+            }
+        },
+    ]
+
+    # now add the GSI to the table
+    table.update(AttributeDefinitions=attrdef, GlobalSecondaryIndexUpdates=recordTypeIdx)
+
+    return table
