7245e49be0a54f9355b435953afc25ce1c6461b3
Karl Schipul <karl@3ringprototype.com>
2022-12-11 16:12:45 -0500

    IRD-1 - switch to accomodate for use of built in 'Function URL'

--- router.py
+++ router.py
@@ -8,51 +8,44 @@ from controllers.Role import Role
 
 def getData(param_id, controller, key_condition_kwargs=None, attribs=[]):
     outval = None
-    
+
     #single, specific record
     if param_id is not None:
         outval = controller.get(param_id)
     else:
         #potentially many records
         outval = controller.getQuery(key_condition_kwargs, attribs)
-        
+
     return outval
-    
+
 
 #determine what the larger overall function shall do, based on the HTTP endpoint
 def route(event):
 
-    #what gets returned by this method
+    # what gets returned by this method
     outval = ""
     
-    #first get the HTTP method, whether it is GET or POST. Other kinds are problematic from browser.
+    # first get the HTTP method, whether it is GET or POST. Other kinds are problematic from browser.
     http_method = event["requestContext"]["http"]["method"]
     
-    #just get the base endpoint
-    base_endpoint = event["routeKey"].replace(http_method + " ", "").replace("/", "").replace("{proxy}", "")
-    
-    #find out if this route has an optional parameter. '0' means it does not.
-    path_parameters = event.get("pathParameters", 0)
-    
-    #if this is a top level endpoint, then there is no meaningful param_id
-    if not path_parameters:
-        param_id = None
-    else:
-        #but if there is one
-        param_id = path_parameters.get("proxy", "")
+    # simple op to get the base_endpoint and the param_id. 'None'... are the defaults to prevent system error
+    base_endpoint, param_id, *_ = [e for e in event["rawPath"].split("/") if e] + [None, None]
 
-    
     #two sets of routes, one based on get, other on post
     if http_method == "GET":
     
-        #now do the actual routing for the GET endpoints
-        if base_endpoint == "getRoles":
+        """
+        now do the actual routing for the GET endpoints.
+        If the param_id is not populated, then call multiple records of that type.
+        All records of that type shall be retrieved if no other parameters are presented.
+        """
+        if base_endpoint == "roles":
             outval = getData(param_id, Role())
-        elif base_endpoint == "getIncidents":
+        elif base_endpoint == "incidents":
             outval = getData(param_id, Incident())
-        elif base_endpoint == "getInstitutions":
+        elif base_endpoint == "institutions":
             outval = getData(param_id, Institution())
-        elif base_endpoint == "getPeople":
+        elif base_endpoint == "people":
             outval = getData(param_id, Person())
         else:
             outval = outval
